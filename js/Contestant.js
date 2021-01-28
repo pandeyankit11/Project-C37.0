6e07c32eda25ca54a0523e0b23fb2a3d48d4c74c
pandeyankit11 <74775903+pandeyankit11@users.noreply.github.com>
2021-01-28 12:39:49 +0530

    Add files via upload

---
+++ js/Contestant.js
@@ -0,0 +1,33 @@
+class Contestant{
+    constructor(){
+
+    }
+
+         
+        getCount(){
+          var contestantCountRef = database.ref('contestantCount');
+          contestantCountRef.on("value",(data)=>{
+            contestantCount = data.val();
+          })
+        }
+      
+        updateCount(count){
+          database.ref('/').update({
+            contestantCount: count
+          });
+        }
+      
+        update(name){
+          var contestantIndex = "contestants/contestant" + this.index;
+          database.ref(contestantIndex).set({
+          name:this.name,
+          
+          });
+        }
+        static getContestantInfo(){
+          var contestantInfoRef=database.ref('contestants');
+          contestantInfoRef.on("value",(data)=>{
+            allContestants=data.val();
+          })
+        }
+    }
